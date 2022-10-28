af93677d65369148bd95f34aee928dcde01addad
nhan <nguyenhoangnhannn93@gmail.com>
2022-10-28 10:43:13 +0700

    php

--- admin/assets/js/login.js
+++ admin/assets/js/login.js
@@ -1,52 +1,52 @@
-"use strict";
-import { Toast, Loading } from "./module/index.js";
+'use strict'
+import { Toast, Loading } from './module/index.js'
 
-const nodeInput = document.querySelectorAll(".form__input");
-nodeInput.forEach((item) => {
-  item.addEventListener("change", (e) => {
+const nodeInput = document.querySelectorAll('.form__input')
+nodeInput.forEach(item => {
+  item.addEventListener('change', e => {
     if (e.target.value.length > 0) {
-      e.target.classList.add("active-input");
+      e.target.classList.add('active-input')
     } else {
-      e.target.classList.remove("active-input");
+      e.target.classList.remove('active-input')
     }
-  });
-});
+  })
+})
 
-$(function () {});
+$(function () {})
 
-$("#signin__form").submit(function (e) {
-  e.preventDefault();
+$('#signin__form').submit(function (e) {
+  e.preventDefault()
 
-  Loading({ urlImg: "./assets/images/loading.gif" });
+  Loading('body', './assets/images/loading1.gif', 'black', '300px', 'center')
 
-  const emailInput = $('input[name="email"]').val();
-  const passwordInput = $('input[name="password"]').val();
+  const emailInput = $('input[name="email"]').val()
+  const passwordInput = $('input[name="password"]').val()
 
   $.ajax({
-    url: "./process-login-admin.php",
-    type: "POST",
+    url: './process-login-admin.php',
+    type: 'POST',
     data: { email: emailInput, password: passwordInput },
-    dataType: "JSON",
+    dataType: 'JSON',
     success(response) {
-      response["login"]
-        ? (window.location.href = "./root/")
+      response['login']
+        ? (window.location.href = './root/')
         : Toast({
-            title: "Thất Bại",
-            msg: "Mật khẩu hoặc email không chính xác",
-            type: "error",
+            title: 'Thất Bại',
+            msg: 'Mật khẩu hoặc email không chính xác',
+            type: 'error',
             duration: 5000,
-          });
+          })
     },
     error(xhr) {
       Toast({
-        title: "Thất Bại",
+        title: 'Thất Bại',
         msg: xhr.responseText,
-        type: "error",
+        type: 'error',
         duration: 5000,
-      });
+      })
     },
     complete() {
-      $(".loading").remove();
+      $('.loading').remove()
     },
-  });
-});
+  })
+})
--- admin/assets/js/module/Loading.js
+++ admin/assets/js/module/Loading.js
@@ -1,17 +1,42 @@
-export default function Loading({ urlImg }) {
-  $("body").append("<div class='loading'></div>");
-  $(".loading").css({
-    content: "",
-    position: "absolute",
-    top: 0,
-    right: 0,
-    bottom: 0,
-    left: 0,
-    "z-index": 1000,
-    "background-color": "rgba(0, 0, 0, 0.6)",
-    "background-image": `url(${urlImg})`,
-    "background-repeat": "no-repeat",
-    "background-position": "center",
-    "background-size": "300px",
-  });
+/**
+ * @param {string} selectors Phần tử để append loading vào
+ * @param {string} urlImg Đường link ảnh loading
+ * @param {string} backgroundColor Chọn nền black hoặc white
+ * @param {string} backgroundSize Kích thước của ảnh
+ * @param {string} backgroundPosition Vị trí của ảnh
+ *
+ * @return {*}
+ */
+'use strict'
+export default function Loading(
+  selectors,
+  urlImg,
+  backgroundColor,
+  backgroundSize,
+  backgroundPosition
+) {
+  if (!$('.loading').length) {
+    const listBackground = {
+      black: 'rgba(0, 0, 0, 0.4)',
+      white: 'rgba(255, 255, 255, 0.6)',
+    }
+    $(selectors).append("<div class='loading'></div>")
+    $(selectors).css({ position: 'relative' })
+    $('.loading').css({
+      content: '',
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      'z-index': 1000,
+      'background-color': 'rgba(255, 255, 255, 0.6)',
+      'background-color': listBackground[backgroundColor],
+      'background-image': `url(${urlImg})`,
+      'background-repeat': 'no-repeat',
+      'background-position': backgroundPosition,
+      'background-size': backgroundSize,
+      'border-radius': '16px',
+    })
+  }
 }
