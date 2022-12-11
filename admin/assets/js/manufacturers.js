7a13ca50d983b88fc3fcf9e4ffb2c54132c5c00f
Nguyễn Hoàng Nhân <nguyenhoangnhannn93@gmail.com>
2022-12-11 11:20:22 +0700

    php

--- admin/assets/js/manufacturers.js
+++ admin/assets/js/manufacturers.js
@@ -5,46 +5,49 @@ import { CallAjax, Loading, Modal, StatusNotification } from './module/index.js'
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
-const controlCheckbox = $('#control__checkbox--all')
+const controlCheckbox = $('#control-checkbox-id')
+const controlReload = $('.control-btn.btn-reload')
+const controlCreate = $('.control-btn.btn-create')
+const controlDelete = $('.control-btn.btn-delete')
 const tbody = $('.table tbody')
-const reloadBtn = $('.control__icon.btn-reload')
-const createBtn = $('.control__icon.btn-create')
 const modalContainer = $('.modal-container')
 
-let checkboxRows = [...$$('.table__col--checkbox')]
+fetch('./load-data.php')
+  .then(response => response.json())
+  .then(data => (App.manufacturers = data))
+  .then(() => App.start())
 
 const App = {
   manufacturers: {},
+  checkboxTable: [],
   fetchManufacturers: function () {
-    const handleData = response => {
-      // this.render(JSON.parse(response))
-      this.manufacturers = response
-      console.log(this.manufacturers)
-    }
-
-    CallAjax({ url: './load-data.php', handleData })
+    //   const handleData = response => {
+    //     this.manufacturers = JSON.parse(response)
+    //     this.render(this.manufacturers)
+    //   }
+    //   CallAjax({ url: './load-data.php', handleData })
   },
-  render: function (response) {
-    if (response.statusCode === 200) {
-      const htmlRows = response.data.map(
+  render: function (data) {
+    if (data.statusCode === 200) {
+      const htmlRows = data.data.map(
         row => `
           <tr>
-            <td class="table__row--center">
-              <label class="table__col flex-center" for="table-col-${row['id']}">
-                <input data-id=${row['id']} type="checkbox" name="" class="table__col--checkbox" id="table-col-${row['id']}">
+            <td class="table-row-center">
+              <label class="table-col flex-center" for="table-col-${row['id']}">
+                <input data-id=${row['id']} type="checkbox" name="" class="table-col-checkbox" id="table-col-${row['id']}">
               </label>
             </td>
-            <td class="table__row--center">${row['id']}</td>
-            <td class="table__row--center">${row['name']}</td>
+            <td class="table-row-center">${row['id']}</td>
+            <td class="table-row-center">${row['name']}</td>
             <td class="vertical-align">${row['address']}</td>
-            <td class="table__row--center">${row['phone']}</td>
-            <td class="table__row--center">
-              <button class="table__col flex-center" title="Chỉnh Sửa" data-id="${row['id']}">
+            <td class="table-row-center">${row['phone']}</td>
+            <td class="table-row-center">
+              <button class="table-col flex-center" title="Chỉnh Sửa" data-id="${row['id']}">
                 <ion-icon name="color-wand"></ion-icon>
               </button>
             </td>
-            <td class="table__row--center">
-              <button class="table__col btn-delete flex-center" title="Xóa" data-type="table" data-id="${row['id']}">
+            <td class="table-row-center">
+              <button class="table-col btn-delete flex-center" title="Xóa" data-type="table" data-id="${row['id']}">
                 <ion-icon name="trash-outline"></ion-icon>
               </button>
             </td>
@@ -55,30 +58,76 @@ const App = {
     }
   },
   // Checkbox All Checked
-  handleCheckedAll: function () {
-    controlCheckbox.onchange = function () {
-      const _this = this
-      checkboxRows.forEach(element => {
-        element.checked = _this.checked
-
-        let idIdx = checkboxCheckeds.ids.indexOf(element.dataset.id)
-        let elementIdx = checkboxCheckeds.elements.indexOf(
-          element.closest('tr')
-        )
-        if (_this.checked === true) {
-          if (idIdx == -1 && elementIdx == -1) {
-            checkboxCheckeds.elements.push(element.closest('tr'))
-            checkboxCheckeds.ids.push(element.dataset.id)
-          }
-        } else {
-          checkboxCheckeds.elements.shift()
-          checkboxCheckeds.ids.shift()
-        }
-      })
-    }
+  handleCheckbox: function () {
+    // this.checkboxTable = [...$$('.table-col-checkbox')]
+    // let checkboxCheckeds = {
+    //   ids: [],
+    //   elements: [],
+    // }
+    // // Checkbox only one Checked
+    // this.checkboxTable.forEach(element => {
+    //   element.onchange = e => {
+    //     const _this = e.target
+    //     if (_this.checked === true) {
+    //       checkboxCheckeds.elements.push(_this.closest('tr'))
+    //       checkboxCheckeds.ids.push(_this.dataset.id)
+    //     } else {
+    //       let idIdx = checkboxCheckeds.ids.indexOf(_this.dataset.id)
+    //       let elementIdx = checkboxCheckeds.elements.indexOf(
+    //         _this.closest('tr')
+    //       )
+    //       checkboxCheckeds.ids.splice(idIdx, 1)
+    //       checkboxCheckeds.elements.splice(elementIdx, 1)
+    //     }
+    //     let cntTrue = 0
+    //     checkboxTable.forEach(element => element.checked && ++cntTrue)
+    //     cntTrue == checkboxTable.length
+    //       ? (controlCheckbox.checked = true)
+    //       : (controlCheckbox.checked = false)
+    //   }
+    // })
   },
-  handleDelete: function () {
-    // ============== Xử lí khi ở trong form vẫn chưa xong ==============
+  handleDeleteBtn: function () {
+    const _this = this
+
+    controlDelete.onclick = function () {
+      const handleSuccess = () => {
+        checkboxCheckeds.elements.forEach(element => {
+          element.remove()
+        })
+      }
+
+      // Show notification when success delete
+      const handleDelete = response => {
+        StatusNotification({
+          response: JSON.parse(response),
+          handleSuccess,
+          subMessage: 'nhà sản xuất',
+        })
+        $('.loading') && $('.loading').remove()
+      }
+
+      // Check valid call ajax do get response data
+      // (status code & status message)
+      if (
+        typeof checkboxCheckeds.ids.length === 'number' &&
+        checkboxCheckeds.ids.length > 0 &&
+        confirm('Bạn muốn xóa những nhà sản xuất bạn đã chọn ???')
+      ) {
+        CallAjax({
+          url: '../manufacturers/process-delete.php',
+          data: { id: checkboxCheckeds.ids },
+          handleData: handleDelete,
+        })
+      } else {
+        if (typeof checkboxCheckeds.ids.length !== 'number')
+          alert('Error: datatype invalid !!')
+        if (checkboxCheckeds.ids.length <= 0)
+          alert('Bạn chưa chọn nhà sản xuất nào !!!')
+        $('.loading') && $('.loading').remove()
+      }
+    }
+
     $$('.btn-delete').forEach(element => {
       element.onclick = function () {
         Loading(
@@ -134,42 +183,6 @@ const App = {
           }
           case 'control': {
             // Handle when success delete
-            const handleSuccess = () => {
-              checkboxCheckeds.elements.forEach(element => {
-                element.remove()
-              })
-              _this.handleEventAgain()
-            }
-
-            // Show notification when success delete
-            const handleDelete = response => {
-              StatusNotification({
-                response: JSON.parse(response),
-                handleSuccess,
-                subMessage: 'nhà sản xuất',
-              })
-              $('.loading') && $('.loading').remove()
-            }
-
-            // Check valid call ajax do get response data
-            // (status code & status message)
-            if (
-              typeof checkboxCheckeds.ids.length === 'number' &&
-              checkboxCheckeds.ids.length > 0 &&
-              confirm('Bạn muốn xóa những nhà sản xuất bạn đã chọn ???')
-            ) {
-              CallAjax({
-                url: '../manufacturers/process-delete.php',
-                data: { id: checkboxCheckeds.ids },
-                handleData: handleDelete,
-              })
-            } else {
-              if (typeof checkboxCheckeds.ids.length !== 'number')
-                alert('Error: datatype invalid !!')
-              if (checkboxCheckeds.ids.length <= 0)
-                alert('Bạn chưa chọn nhà sản xuất nào !!!')
-              $('.loading') && $('.loading').remove()
-            }
 
             break
           }
@@ -181,45 +194,32 @@ const App = {
       }
     })
   },
-  handleEventAgain: function () {
+  handleEvent: function () {
     const _this = this
-    checkboxRows = [...$$('.table__col--checkbox')]
+    this.checkboxTable = [...$$('.table-col-checkbox')]
 
-    let checkboxCheckeds = {
-      ids: [],
-      elements: [],
+    controlCheckbox.onchange = function () {
+      const __this = this
+      _this.checkboxTable.forEach(element => {
+        element.checked = __this.checked
+
+        let idIdx = checkboxCheckeds.ids.indexOf(element.dataset.id)
+        let elementIdx = checkboxCheckeds.elements.indexOf(
+          element.closest('tr')
+        )
+        if (__this.checked === true) {
+          if (idIdx == -1 && elementIdx == -1) {
+            checkboxCheckeds.elements.push(element.closest('tr'))
+            checkboxCheckeds.ids.push(element.dataset.id)
+          }
+        } else {
+          checkboxCheckeds.elements.shift()
+          checkboxCheckeds.ids.shift()
+        }
+      })
     }
 
-    // Checkbox only one Checked
-    checkboxRows.forEach(element => {
-      element.onchange = e => {
-        const _this = e.target
-        if (_this.checked === true) {
-          checkboxCheckeds.elements.push(_this.closest('tr'))
-          checkboxCheckeds.ids.push(_this.dataset.id)
-        } else {
-          let idIdx = checkboxCheckeds.ids.indexOf(_this.dataset.id)
-          let elementIdx = checkboxCheckeds.elements.indexOf(
-            _this.closest('tr')
-          )
-          checkboxCheckeds.ids.splice(idIdx, 1)
-          checkboxCheckeds.elements.splice(elementIdx, 1)
-        }
-
-        let cntTrue = 0
-        checkboxRows.forEach(element => element.checked && ++cntTrue)
-        cntTrue == checkboxRows.length
-          ? (controlCheckbox.checked = true)
-          : (controlCheckbox.checked = false)
-      }
-    })
-  },
-  handleEvent: function () {
-    this.handleEventAgain()
-
-    const _this = this
-
-    reloadBtn.onclick = function () {
+    controlReload.onclick = function () {
       Loading(
         '.table',
         '../assets/img/loading2.gif',
@@ -242,7 +242,7 @@ const App = {
       })
     }
 
-    createBtn.onclick = function () {
+    controlCreate.onclick = function () {
       modalContainer.style.display = 'block'
 
       let formData = {}
@@ -250,22 +250,22 @@ const App = {
       const handleSuccess = () => {
         const htmlRows = document.createElement('tr')
         htmlRows.innerHTML = `
-          <td class="table__row--center">
-            <label class="table__col flex-center" for="table-col-${formData['idInsert']}">
-              <input data-id="${formData['idInsert']}" type="checkbox" name="" class="table__col--checkbox" id="table-col-${formData['idInsert']}">
+          <td class="table-row-center">
+            <label class="table-col flex-center" for="table-col-${formData['idInsert']}">
+              <input data-id="${formData['idInsert']}" type="checkbox" name="" class="table-col-checkbox" id="table-col-${formData['idInsert']}">
             </label>
           </td>
-          <td class="table__row--center">${formData['idInsert']}</td>
-          <td class="table__row--center">${formData['name']}</td>
+          <td class="table-row-center">${formData['idInsert']}</td>
+          <td class="table-row-center">${formData['name']}</td>
           <td class="vertical-align">${formData['address']}</td>
-          <td class="table__row--center">${formData['phone']}</td>
-          <td class="table__row--center">
-            <button class="table__col flex-center" title="Chỉnh Sửa" href="../manufacturers/form_update.php?id=${formData['idInsert']}">
+          <td class="table-row-center">${formData['phone']}</td>
+          <td class="table-row-center">
+            <button class="table-col flex-center" title="Chỉnh Sửa" href="../manufacturers/form_update.php?id=${formData['idInsert']}">
               <ion-icon name="color-wand" role="img" class="md hydrated" aria-label="color wand"></ion-icon>
             </button>
           </td>
-          <td class="table__row--center">
-            <button class="table__col btn-delete flex-center" title="Xóa" data-type="table" data-id="${formData['idInsert']}">
+          <td class="table-row-center">
+            <button class="table-col btn-delete flex-center" title="Xóa" data-type="table" data-id="${formData['idInsert']}">
               <ion-icon name="trash-outline" role="img" class="md hydrated" aria-label="trash outline"></ion-icon>
             </button>
           </td>
@@ -323,10 +323,13 @@ const App = {
     }
   },
   start: function () {
-    this.fetchManufacturers()
+    // this.fetchManufacturers()
+    // this.handleCheckbox()
+    // this.handleEvent()
 
+    this.render(this.manufacturers)
     this.handleEvent()
   },
 }
 
-App.start()
+// App.start()
--- admin/manufacturers/index.php
+++ admin/manufacturers/index.php
@@ -5,7 +5,7 @@
 <head>
 	<?php require '../header-tag.php' ?>
 	<link rel="stylesheet" href="../assets/css/manufacturers.css">
-	<script defer type="module" src="../assets/js/App.js"></script>
+	<script defer type="module" src="../assets/js/app.js"></script>
 	<script defer type="module" src="../assets/js/manufacturers.js"></script>
 	<title>Giao Diện Admin</title>
 </head>
@@ -24,43 +24,43 @@
 			<?php require '../header.php' ?>
 			<main id="main">
 				<div class="container">
-					<div class="content-header">
-						<h1 id="title">Nhà Sản Xuất</h1>
+					<div id="content-header">
+						<h1>Nhà Sản Xuất</h1>
 					</div>
-					<section id="content">
-						<div class="control">
-							<nav class="control__feature">
+					<div id="content-body">
+						<section id="section-controller">
+							<nav class="control-features">
 								<ul>
 									<li>
-										<label title="Chọn Tất Cả" for="control__checkbox--all" class="control__checkbox">
-											<input type="checkbox" name="" id="control__checkbox--all">
+										<label title="Chọn Tất Cả" for="control-checkbox-id" class="control-checkbox">
+											<input type="checkbox" name="" id="control-checkbox-id">
 										</label>
 									</li>
 									<li>
-										<button title="Thêm" class="control__icon btn-create">
+										<button title="Thêm" class="control-btn btn-create">
 											<ion-icon name="create-outline"></ion-icon>
 										</button>
 									</li>
 									<li>
-										<button title="Tải Lại" class="control__icon btn-reload" data-type="control">
+										<button title="Tải Lại" class="control-btn btn-reload">
 											<ion-icon name="reload-outline"></ion-icon>
 										</button>
 									</li>
 									<li>
-										<button title="Xóa Mục Đã Chọn" class="control__icon btn-delete" data-type="control">
+										<button title="Xóa Mục Đã Chọn" class="control-btn btn-delete">
 											<ion-icon name="trash-outline"></ion-icon>
 										</button>
 									</li>
 								</ul>
 							</nav>
-							<div class="control__search">
+							<div class="control-search">
 								<input type="search" name="search" placeholder="Tìm kiếm">
 							</div>
-						</div>
-						<div class="table-content">
+						</section>
+						<section id="section-table">
 							<table class="table">
 								<thead>
-									<tr class="table__thead--bg-primary">
+									<tr class="table-bg-primary">
 										<th>Chọn</th>
 										<th>Mã</th>
 										<th>Tên</th>
@@ -72,8 +72,8 @@
 								</thead>
 								<tbody></tbody>
 							</table>
-						</div>
-					</section>
+						</section>
+					</div>
 				</div>
 			</main>
 		</div>
