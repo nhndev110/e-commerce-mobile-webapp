a61051d73a38791fd19754aa4013915687a1f30b
nhndev110 <nguyenhoangnhannn93@gmail.com>
2023-04-06 10:56:56 +0700

    update

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controllers;
 
-class BaseController {
-
+class BaseController
+{
+	
 }
--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,18 +2,12 @@
 
 namespace App\Controllers;
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once '.\vendor\autoload.php';
 
 $klein = new \Klein\Klein();
 
 class HomeController extends BaseController
 {
-  public function __construct()
-  {
-//    require_once './app/views/client/index.php';
-//    $service->render('./app/views/client/index.php', array('title' => 'My View'));
-  }
-
   public function index()
   {
     // $klein->respond()
