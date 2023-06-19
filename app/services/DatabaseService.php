644bf9b6bafb4fed823e689178f0ee667ee45bca
nhndev110 <nguyenhoangnhannn93@gmail.com>
2023-06-19 23:02:42 +0700

    transfer database.php to DatabaseService.php

---
+++ app/services/DatabaseService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services;
+
+require_once "./config/DatabaseConfig.php";
+
+use mysqli;
+use mysqli_sql_exception;
+use Config\DatabaseConfig;
+
+class Database
+{
+  private string $localhost;
+  private string $username;
+  private string $password;
+  private string $database;
+
+  private object $connect;
+
+  public function __construct()
+  {
+    $this->localhost = $_ENV['DB_HOST'];
+    $this->username = $_ENV['DB_USER'];
+    $this->password = $_ENV['DB_PASS'];
+    $this->database = $_ENV['DB_DATABASE'];
+  }
+
+  public function DBConnect(): void
+  {
+    try {
+      $this->connect = new mysqli($this->localhost, $this->username, $this->password, $this->database);
+      $this->connect->set_charset('utf8mb4');
+
+      if ($this->connect->connect_error) {
+        error_log($this->connect->connect_error);
+      }
+    } catch (mysqli_sql_exception $e) {
+      error_log($e->__toString());
+    }
+  }
+
+  /**
+   * @param string $sql
+   * @return array
+   */
+  public function executeQuery(string $sql): array
+  {
+    $res = $this->connect->query($sql);
+    $data = [];
+
+    $rows = $res->fetch_all(MYSQLI_ASSOC);
+    foreach ($rows as $row) {
+      $data[] = $row;
+    }
+
+    $res->free();
+
+    return $data;
+  }
+
+  public function executeUpdate(string $sql): void
+  {
+    mysqli_query($this->connect, $sql);
+  }
+
+  public function close(): void
+  {
+    $this->connect->close();
+  }
+}
